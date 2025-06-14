b68abbf51fa2078f7f9783e899e68bd9f99faf3a
Piotr Kapitkowski <japiotr123@gmail.com>
2025-06-14 18:37:48 +0200

    Naprawa buga przy przełączaniu na kalendarz
    Pzełączanie z kontrolki umów wizytę na kalendarz skutkowało crashowaniem
    aplikacji, błąd jest związany z UIElementAutomationPeer.
    Możliwa przyczyna - bląd w Microsoft WPF
    dotnet/wpf/issues/8188

---
+++ Controls/CalendarNoUia.cs
@@ -0,0 +1,16 @@
+﻿using System.Windows.Automation.Peers;
+using System.Windows.Controls;
+
+namespace PolMedUMG.Controls
+{
+    /// <summary>
+    /// Kalendarz bez obsługi UI-Automation 
+    /// zapobiega crashowania aplikacji po zmianie ustawienia
+    /// kontrolki w "umów wizytę" i przełączeniu na kalendarz
+    /// błąd prawdopodobnie związany z istniejącym zgłoszeniem https://github.com/dotnet/wpf/issues/8188
+    /// </summary>
+    public class CalendarNoUia : Calendar
+    {
+        protected override AutomationPeer OnCreateAutomationPeer() => null;
+    }
+}
